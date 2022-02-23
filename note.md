e6bc0ec479b989502fdc65777e219681b52f8d9a
sa46lll <9812smh@naver.com>
2022-02-23 11:49:05 +0900

    feat: Logout 처리

--- note.md
+++ note.md
@@ -3,6 +3,11 @@
 > 인증 & 인가
 
 - 인증
+  - 유저가 누구인지 확인하는 절차, 회원가입하고 로그인 하는 것.
+    - 방문자가 자신이 회사 건물에 들어 갈 수있는지 확인 받는 과정
+- 인가
+  - 유저에 대한 권한을 허락하는 것.
+    - 방문자가 회사 건물에 방문했을 때, 허가된 공간에만 접근 가능
 
 
 > 인증 API
@@ -22,5 +27,17 @@
     }
     ```
   
+- Logout
+  ```java
+  protected void configure(HttpSecurity http) throws Exception {
+       http.logout()						// 로그아웃 처리
+               .logoutUrl("/logout")				// 로그아웃 처리 URL
+               .logoutSuccessUrl("/login")			// 로그아웃 성공 후 이동페이지
+               .deleteCookies("JSESSIONID", "remember-me") 	// 로그아웃 후 쿠키 삭제
+               .addLogoutHandler(logoutHandler())		        // 로그아웃 핸들러
+               .logoutSuccessHandler(logoutSuccessHandler()) 	// 로그아웃 성공 후 핸들러
+  }
+  ```
+
 
 
--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -44,6 +47,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll();   // 누구나 접근 가능하게 함.
+                .permitAll()   // 누구나 접근 가능하게 함.
+                ;
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();   // 세션 무효화
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me")   // remember-me 인증의 쿠키를 제거
+                ;
     }
 }
